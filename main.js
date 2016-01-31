fc18755806f4fc74c5c3a931fe7131dbf4913080
David_Stichter <david.stichter3414@gmail.com>
2016-01-31 18:00:51 -0500

    Fixed scope issues, Functions calling the wrong functions, variable name issues

--- main.js
+++ main.js
@@ -26,10 +26,9 @@ var zoo = {
   add: function(input_scope){
     var currentScope = input_scope;
     console.log("To Add animal, fill out form")
-    prompt.get(['name','type','age'],function(err, result){
-      var query = 'INSERT INTO animals (name,type,age) VALUES (?,?,?)'
-      var toAdd = [result.name,result.type,result.age]
-      connection.connect();
+    prompt.get(['name','type','age','caretaker'],function(err, result){
+      var query = 'INSERT INTO animals (name,type,age,caretaker_id) VALUES (?,?,?,?)'
+      var toAdd = [result.name,result.type,result.age,result.caretaker]
       connection.query(query, toAdd, function(err, results) {
         if(err) throw err;
         console.log("Added");
@@ -45,14 +44,14 @@ var zoo = {
     console.log("Enter (C): ----- here's the count for all animals in this one city!");
     console.log("Enter (O): ----- here's the count for all the animals in all locations by the type you specified!");
     console.log("Enter (Q): ----- Quits to the main menu!");
-    this.view()
+    this.view(this)
   },
   view: function(input_scope){
     var currentScope = input_scope;
     prompt.get(['visit'],function(err,result){
       if(result.visit === "Q"){
         currentScope.menu();
-        currentScope.promtpUser()
+        currentScope.promptUser()
       }
       else if(result.visit === "O"){
         currentScope.type(currentScope);
@@ -83,20 +82,21 @@ var zoo = {
         if(err) throw err;
         console.log(results);
         currentScope.visit()
-        currentScope.view(currentScope)
+        
       })
     });
   },
   care: function(input_scope){
     console.log("Enter City NY/SF")
+    var currentScope = input_scope;
     prompt.get(['city_name'], function(err, results){
       var query = 'SELECT COUNT(*) FROM animals,caretakers WHERE caretakers.city=? AND caretakers.id=animals.caretaker_id'
-      var inputType = result.city_name;
+      var inputType = results.city_name;
       connection.query(query, inputType, function(err, results) {
         if(err) throw err;
         console.log(results);
         currentScope.visit()
-        currentScope.view(currentScope)
+        
       })
     });
   },
@@ -105,12 +105,12 @@ var zoo = {
     console.log("Enter ID of Animal")
     prompt.get(['animal_id'],function(err,results){
       var query = 'SELECT * FROM animals WHERE id=?'
-      var inputType = result.animal_id;
+      var inputType = results.animal_id;
       connection.query(query, inputType, function(err, results) {
         if(err) throw err;
         console.log(results);
         currentScope.visit()
-        currentScope.view(currentScope)
+        
       })
     });
   },
@@ -119,22 +119,23 @@ var zoo = {
     console.log("Enter Name of Animal")
     prompt.get(['animal_name'],function(err,results){
       var query = 'SELECT * FROM animals WHERE name=?'
-      var inputType = result.animal_name;
+      var inputType = results.animal_name;
       connection.query(query, inputType, function(err, results) {
         if(err) throw err;
         console.log(results);
         currentScope.visit()
-        currentScope.view(currentScope)
+        
       })
     });
   },
   all: function(input_scope){
+    var currentScope = input_scope;
       var query = 'SELECT COUNT(*) FROM animals'
       connection.query(query, function(err, results) {
         if(err) throw err;
         console.log(results);
         currentScope.visit()
-        currentScope.view(currentScope)
+        
       })
   },
   update: function(input_scope){
@@ -170,15 +171,15 @@ var zoo = {
         self.exit()
       }
       else if(results.input === "A"){
-        self.all(self)
+        self.add(self)
       }
       else if(results.input === "V"){
         self.visit()
       }
       else if(results.input === "D"){
-        self.view(self)
+        self.adopt(self)
       }
-      else if(result.visit === "U"){
+      else if(results.input === "U"){
         self.update(self);
       }
       else{
